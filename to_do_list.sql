98fb32ab85221c64b1d063593455b2be69e8e6b9
RATHNA KUMAR <kumarrathna1799@gmail.com>
2019-12-26 11:57:37 +0530

    Update to_do_list.sql

--- to_do_list.sql
+++ to_do_list.sql
@@ -4,12 +4,19 @@ task_number number not null,
 task_name varchar2(50) not null,
 task_by varchar2(30) not null,
 dead_line date,
-status char(1),
+status char(1) default 'N',
 completed_on date,
+priority_number number not null,
+created_date date default SYSDATE,
+modified_date date not null,
 constraint task_uq unique(task_name),
 constraint task_number_pk primary key(task_number),
 constraint task_number_uq unique(task_number),
 constraint dead_line_ck check(dead_line > SYSDATE),
 constraint status_ck check(status in ('Y','N,)),
+constraint modified_date_ck check(modified_date > created_date),
 );
+insert into todolist(task_number,task_name,task_by,dead_line,priority_number)
+value(1,'download redbus','sivanadhan',to date('28.12.2019','DD.MM.YYYY'),1);
+selecr *from todolist;
 
